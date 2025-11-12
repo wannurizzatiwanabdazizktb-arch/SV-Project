ac51b773746872e49ab9bde5fae1e73160ec33eb
Wan Izzati <wannurizzatiwanabdazizktb@gmail.com>
2025-11-12 14:11:01 +0800

    Update GoogleFormData.py

--- GoogleFormData.py
+++ GoogleFormData.py
@@ -1,19 +1,6 @@
 import streamlit as st
-import gspread
 import pandas as pd
-from oauth2client.service_account import ServiceAccountCredentials
 
-scope = ["https://spreadsheets.google.com/feeds", 
-         "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
-client = gspread.authorize(creds)
-
-@st.cache_data(ttl=30)
-def load_data():
-    sheet = client.open_by_key("1kYT50a_poiLoHg2gd3WzoRfN8EGs72ROHXGm7NTPXeA").sheet1
-    data = sheet.get_all_records()
-    df = pd.DataFrame(data)
-    return df
-
-df = load_data()
+url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS8nPPwgVKnGxpQLQFTH6EQLpO6l1l2BlEAdGqmb0Bq7FGQzViLwKbb78NMjJSA1-eHl-Ebq5Wl4LRU/pub?gid=745446698&single=true&output=csv"
+df = pd.read_csv(url)
 st.dataframe(df)

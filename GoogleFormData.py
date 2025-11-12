d724773c1a0a8c1004c5178e5fe0b948c93890a1
Wan Izzati <wannurizzatiwanabdazizktb@gmail.com>
2025-11-12 14:24:27 +0800

    Update GoogleFormData.py

--- GoogleFormData.py
+++ GoogleFormData.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 
+st.header("Survey Dataset: Public Opinions on School Traffic Congestion During Peak Hours")
 url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS8nPPwgVKnGxpQLQFTH6EQLpO6l1l2BlEAdGqmb0Bq7FGQzViLwKbb78NMjJSA1-eHl-Ebq5Wl4LRU/pub?gid=745446698&single=true&output=csv"
 df = pd.read_csv(url)
 

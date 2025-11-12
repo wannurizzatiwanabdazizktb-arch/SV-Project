a8da0ebf84476b336f24f29475b35e50231e26f2
Wan Izzati <wannurizzatiwanabdazizktb@gmail.com>
2025-11-12 14:21:42 +0800

    Update GoogleFormData.py

--- GoogleFormData.py
+++ GoogleFormData.py
@@ -3,4 +3,8 @@ import pandas as pd
 
 url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS8nPPwgVKnGxpQLQFTH6EQLpO6l1l2BlEAdGqmb0Bq7FGQzViLwKbb78NMjJSA1-eHl-Ebq5Wl4LRU/pub?gid=745446698&single=true&output=csv"
 df = pd.read_csv(url)
+
+# Exclude columns by name
+df = df.drop(columns=["Timestamp", "Score"])
+
 st.dataframe(df)

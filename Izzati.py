dd3ff6f3a827f88c68c3a2d10c77e4d80a06d09c
Wan Izzati <wannurizzatiwanabdazizktb@gmail.com>
2026-01-04 02:30:05 +0800

    Update Izzati.py

--- Izzati.py
+++ Izzati.py
@@ -1,1 +1,18 @@
+# To explore the relationship between traffic factors and congestion effect from rural perspectives.
 
+import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+import numpy as np
+
+# Load Dataset
+
+# Bar Chart: Ranking of factor that caused trafic congestion.
+
+# Pie Chart: Percentage distribution of effect from the traffic congestion.
+
+# Heatmap: Traffic Factors vs Congestion Effects.
+
+# Radar Chart: Percentage score of effect from one factor.
+
+# Box Plot: Congestion Effect by Severity of a Key Traffic Factor

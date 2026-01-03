4514c401247bae0abf99d297eebcf59b76fa2b4a
s22a0064-AinMaisarah <s22a0064@siswa.umk.edu.my>
2026-01-04 02:20:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+import streamlit as st
 
+st.set_page_config(page_title="Traffic Congestion During Peak Hours in Front of Schools Dashboard")
+
+# Import pages
+page1 = st.Page("Disagreement_Traffic.py", title="Disagreement Traffic Congestion Survey")
+page2 = st.Page("Izzati.py", title="Traffic Congestion Survey")
+page3 = st.Page("Fathin.py", title="Traffic Congestion Survey")
+page3 = st.Page("Khalida.py", title="Traffic Congestion Survey")
+
+# Navigation
+navigation = st.navigation(
+    {
+        "Traffic Congestion During Peak Hours in Front of Schools Dashboard": [page1, page2, page3, page4]
+    }
+)
+
+navigation.run()
